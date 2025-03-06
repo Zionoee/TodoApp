085ca0840ee3ef38fedf533ad57bac0ea0c22079
zionee <miracohene@gmail.com>
2025-03-06 04:32:58 -0800

    OPTIMIZED MODAL

--- src/pages/Todo/TodoDetailsModal.tsx
+++ src/pages/Todo/TodoDetailsModal.tsx
@@ -45,15 +45,15 @@ setEdit(value)
      opacity: [1,0]
     }
   }
-  console.log("modal ran")
     return(<AnimatePresence>
+        <div className=" z-40 inset-x-0 fixed top-[20dvh] flex justify-center items-center">
         <motion.div 
     initial = {DetailsAnimation.initial}
     animate = {DetailsAnimation.animate}
     transition={DetailsAnimation.transition}
     exit = {DetailsAnimation.exit}
-    className=" z-40 text-yellow-600 rounded-t-xl fixed top-[20dvh]  pt-10 gap-y-3 flex flex-col items-center min-w-80 max-w-85 min-h-100  bg-stone-600 sm:scale-115 md:scale-125 md:top-45 lg:w-90">
-    <span onClick={()=> {setShowDetails(false)}} className="cursor-pointer absolute right-8 top-6 text-2xl font-bold transition-all duration-300 hover:text-red-700 hover:scale-110 ">X</span>
+    className="  text-yellow-600 rounded-t-xl  relative pt-10 gap-y-3 flex flex-col items-center min-w-70 max-w-85 min-h-100  bg-stone-600 sm:scale-115 md:scale-125 md:top-45 lg:w-90">
+    <span onClick={()=> {setShowDetails(false)}} className=" cursor-pointer absolute right-8 top-4 text-2xl font-bold transition-all duration-300 hover:text-red-700 hover:scale-110 ">X</span>
     <p className="text-zinc-800 text-2xl font-semibold font-serif tracking-wider">{selectedTodo.data.Title}</p>
     <div className="border-t-1 border-t-green-400 w-full flex-1">
       <textarea
@@ -73,6 +73,7 @@ setEdit(value)
     <button onClick = {handleDelete}className="cursor-pointer transition-all duration-300 hover:scale-110 hover:bg-red-700  font-semibold  text-zinc-800 px-5 mr-10 rounded-full bg-amber-900">Delete</button>
     </div>
     </motion.div>
+        </div>
     </AnimatePresence>)
 }
 // ${showDetailsModal && "blur-[3px] opacity-50"}
